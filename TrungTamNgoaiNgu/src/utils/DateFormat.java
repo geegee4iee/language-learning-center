e74b2489f28a82e9ce634d601102a7f9c86e495c
keikiyoshi <huynhchanhkiet94@gmail.com>
2015-06-16 12:05:47 +0700

    update

--- TrungTamNgoaiNgu/src/utils/DateFormat.java
+++ TrungTamNgoaiNgu/src/utils/DateFormat.java
@@ -78,7 +78,7 @@ public class DateFormat {
 	}
 
 	public Date getDateTime(String dtStr) {
-		SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/yyyy h:mm aa");
+		SimpleDateFormat sdf = new SimpleDateFormat(FORMAT_3);
 		Date dateNew = new Date();
 
 		try {
